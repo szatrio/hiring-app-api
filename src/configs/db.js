c72288b20a045309fcb2833abfa04697e2241bdb
Satrio Utomo <szatrio@gmail.com>
2020-02-20 17:42:12 +0700

    Update db.js

--- src/configs/db.js
+++ src/configs/db.js
@@ -1,36 +1,18 @@
 const mysql = require ('mysql');
 
 const db_config = {
+  connectionLimit : 10,
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
 }
 
+const pool  = mysql.createPool(db_config);
 
-let connection
+pool.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+  if (error) throw error;
+  console.log('The solution is: ', results[0].solution);
+});
 
-const handleDisconnect = () => {                                                 // the old one cannot be reused.
-  
-  connection = mysql.createConnection(db_config)
-
-  connection.connect(function(err) {              // The server is either down
-    if(err) {                                     // or restarting (takes a while sometimes).
-      console.log('error when connecting to db:', err);
-      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
-    }                                     // to avoid a hot loop, and to allow our node script to
-  });                                     // process asynchronous requests in the meantime.
-                                          // If you're also serving http, display a 503 error.
-  connection.on('error', function(err) {
-    console.log('db error', err);
-    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
-      handleDisconnect();                         // lost due to either server restart, or a
-    } else {                                      // connnection idle timeout (the wait_timeout
-      throw err;                                  // server variable configures this)
-    }
-  });
-}
-
-handleDisconnect();
-
-module.exports = connection
+module.exports = pool
